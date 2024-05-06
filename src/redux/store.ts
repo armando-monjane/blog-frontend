93b33fa41d655a9275add0313c4233421c449062
Armando Monjane <armando.f.monjane@gmail.com>
2024-05-06 13:36:46 +0200

    removed unecessary reducers

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,10 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
-import pokemonReducer from './features/pokemon/pokemon-slice';
-import feedbackSlice from './features/global/feedback-slice';
-
 import { api } from '@services/index';
 
 const reducer = combineReducers({
     [api.reducerPath]: api.reducer,
-    pokemon: pokemonReducer,
-    feedback: feedbackSlice,
 });
 
 export const store = configureStore({
